46930295916a969744c6c1cb7c40897314398c67
Andhika Gilang Rahadian <rahadian.kidz92@gmail.com>
2025-09-04 22:41:40 +0700

    Update all changes: UI, assets, packages, and other files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navbarNav = document.querySelector(".navbar-nav");
+//homburger menu klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
